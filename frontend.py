7b80a67c7fc9083c5ed393caafdba27fc4801ea9
Luchia Ivanova <luchiya.d.i@gmail.com>
2024-10-08 22:23:12 +0300

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,21 +1,44 @@
 # Luchia
 from backend import *
 
+cars = [
+    {"id": 1, "brand": "Toyota", "model": "Camry", "rental_price": 40, "available": "True", "current_km": "0"},
+    {"id": 2, "brand": "Ford", "model": "F150", "rental_price": 60, "available": "True", "current_km": "0"},
+    {"id": 3, "brand": "Opel", "model": "Insignia", "rental_price": 35, "available": "True", "current_km": "0"}
+    ]
+
 
 def check_available_cars():
-    pass
+    print(f'Available cars:')
+    for car in cars:
+        status = 'Available' if car['available'] else 'Rented'
+        print(f"{car['id']}: {car['brand']} {car['model']} - {status}")
 
 
-def rent_car():
-    pass
+def rent_car(_cars, car_id):
+    for car in cars:
+        if car["id"] == car_id:
+            if car["available"]:
+                car["available"] = False
+                return f"You rented {car['brand']} {car['model']}."
+            else:
+                return "The car is already rented."
+    return "Unavailable car ID."
 
 
-def return_car():
-    pass
+def return_car(__cars, car_id):
+    for car in cars:
+        if car["id"] == car_id:
+            if not car["available"]:
+                car["available"] = True
+                return f"You returned {car['brand']} {car['model']}."
+            else:
+                return "The car is already available."
+    return "Unavailable car ID."
 
 
 def display_menu():
-    print("Welcome to RentACar!")
+    print("\nWelcome to RentACar!")
     print("1. View Cars")
     print("2. Rent a Car")
     print("3. Return a Car")
@@ -25,27 +48,28 @@ def display_menu():
 
 
 def admin_menu():
-    print("Welcome to RentACar admin console!")
+    print("\nWelcome to RentACar admin console!")
     print("1. Add Car")
     print("2. Delete Car")
     print("3. Return to main menu")
     return input("Please choose an option (1-3): ")
 
 
-def main():
+def main_menu():
     while True:
         choice = display_menu()
 
         if choice == '1':
             check_available_cars()
-        if choice == '2':
-            rent_car()
-        if choice == '3':
-            return_car()
-        if choice == '4':
+        elif choice == '2':
+            car_id = int(input("Enter the car ID you want to rent: "))
+            print(rent_car(cars, car_id))
+        elif choice == '3':
+            car_id = int(input("Enter the car ID you want to return: "))
+            print(return_car(cars, car_id))
+        elif choice == '4':
             admin_menu()
-        if choice == '5':
+        elif choice == '5':
             exit('You exited the program.')
         else:
-            print('Invalid option. Please try again:')
-
+            print('Invalid option. Please try again.')
