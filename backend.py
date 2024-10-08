dc500528d07918322164b6aaddd1240f2c46f524
Tsvetomir Penchev <penchevtsvetomir@gmail.com>
2024-10-08 20:54:03 +0300

    Add frontend and backend initial data

--- backend.py
+++ backend.py
@@ -1,1 +1,26 @@
 # Denis
+def add_car():
+    pass
+
+
+def delete_car():
+    pass
+
+
+def modify_car():
+    pass
+
+
+def rent_car():
+    pass
+
+
+def return_car():
+    pass
+
+
+def calculate_total_cost():
+    pass
+
+
+
--- frontend.py
+++ frontend.py
@@ -1,1 +1,37 @@
 # Luchia
+from backend import *
+
+def check_available_cars():
+    pass
+
+def rent_car():
+    pass
+
+
+def return_car():
+    pass
+
+
+def display_menu():
+    print("Welcome to RentACar!")
+    print("1. View Cars")
+    print("2. Rent a Car")
+    print("3. Return a Car")
+    print("5. Administrator")
+    print("4. Exit")
+    return input("Please choose an option (1-5): ")
+
+
+def admin_menu():
+    print("Welcome to RentACar admin console!")
+    print("1. Add Car")
+    print("2. Delete Car")
+    print("3. Return to main menu")
+    return input("Please choose an option (1-3): ")
+
+
+def main():
+    choice = display_menu()
+    pass
+
+
