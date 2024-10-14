fdd4acf480a4df654e0979eeee64038c17919cb9
Tsvetomir Penchev <penchevtsvetomir@gmail.com>
2024-10-14 22:42:41 +0300

    Correction of few options

--- frontend.py
+++ frontend.py
@@ -1,14 +1,17 @@
 # Luchia
-from backend import initialize_cars, add_car, remove_car, edit_car, rent_car, return_car, calculate_total
+import time
+
+from backend import add_car, remove_car, edit_car, rent_car, return_car, calculate_total, CARS
 
 admin_pass = 'admin123'
 
 
 def check_available_cars():
     print(f'Available cars:')
-    for car in cars:
+    for car in CARS:
         status = 'Available' if car['available'] else 'Rented'
-        print(f"{car['id']}: {car['brand']} {car['model']} - {status}")
+        print(f"{car['id']}: {car['brand']} {car['model']} - Status: {status} - Mileage: {car['mileage']}")
+        time.sleep(0.5)
 
 
 def display_menu():
@@ -25,7 +28,8 @@ def admin_menu():
     print("\nWelcome to RentACar admin console!")
     print("1. Add Car")
     print("2. Delete Car")
-    print("3. Return to main menu")
+    print("3. Edit car")
+    print("4. Return to main menu")
     return input("Please choose an option (1-3): ")
 
 
@@ -37,20 +41,25 @@ def main_menu():
             check_available_cars()
         elif choice == '2':
             car_id = int(input('Enter the car ID you want to rent: '))
-            print(rent_car(cars, car_id))
+            print(rent_car(car_id))
         elif choice == '3':
             car_id = int(input('Enter the car ID you want to return: '))
-            print(return_car(cars, car_id))
+            new_millage = int(input("Enter the current mileage of the car: "))
+            print(return_car(car_id))
+            print(calculate_total(car_id, new_millage))
+
         elif choice == '4':
             print('Enter the admin password:')
             password = input()
             if password == admin_pass:
                 command = admin_menu()
                 if command == '1':
-                    add_car()
+                    print(add_car())
                 elif command == '2':
-                    remove_car()
+                    print(remove_car())
                 elif command == '3':
+                    print(edit_car())
+                elif command == '4':
                     main_menu()
             else:
                 print('Incorrect password, access denied.\nReturning to main menu.')
